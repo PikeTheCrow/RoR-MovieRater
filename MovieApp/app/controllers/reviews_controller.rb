4954b1d0203cdb5099b67abf3e9ca50963312dfd
PikeTheCrow <pikethecrow@gmail.com>
2015-12-28 19:18:19 +0000

    added raty js for star rating

--- MovieApp/app/controllers/reviews_controller.rb
+++ MovieApp/app/controllers/reviews_controller.rb
@@ -1,17 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
-  
+
   respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
 
   def new
     @review = Review.new
@@ -24,9 +16,10 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     @review.save
-    respond_with(@review)
+    respond_with(@movie)
   end
 
   def update
@@ -42,6 +35,10 @@ class ReviewsController < ApplicationController
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     def review_params
